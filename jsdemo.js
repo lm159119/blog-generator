33320d4bc3258f64da8ed0f384cbbcc232e20a98
lm159119 <35952462+lm159119@users.noreply.github.com>
2018-02-02 17:32:06 +0800

    Add files via upload

--- jsdemo.js
+++ jsdemo.js
@@ -9,9 +9,9 @@ var reqPath=process.cwd()+"\\"+dirName;
             process.chdir("./" + dirName) 
             fs.mkdirSync('css')
             fs.mkdirSync('js')    
-            fs.writeFileSync("./index.html", "")
-            fs.writeFileSync("css/style.css", "")
-            fs.writeFileSync("./js/main.js", "")   
+            fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>")
+            fs.writeFileSync("css/style.css", "h1{color: red;}")
+            fs.writeFileSync("./js/main.js", "var string = 'Hello World';\n alert(string)")   
             process.exit(0)
             
         }else{
